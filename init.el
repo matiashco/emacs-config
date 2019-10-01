071ff0b8a8e899e7368ad706373cdef2d4e2446e
Matias Hanco <matiash@mit.edu>
2019-09-30 23:20:41 -0500

    Push current emacs config

---
+++ init.el
@@ -0,0 +1,31 @@
+;; Uncomment this line if you're starting with a fresh emacs
+;; (package-initialize)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("mepla" . "https://melpa.org/packages/"))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "myinit.org"
+                   user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/Dropbox/org/personal/journal.org"))
+ '(package-selected-packages
+   '(dumb-jump mu mu4e try org-pdfview pdf-tools use-package modern-cpp-font-lock flycheck eyebrowse atom-one-dark-theme ag ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
